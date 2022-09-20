75cc748d76f997a865e9c48284f131052ab44582
Jonathan Ochaya <wangolojowel@gmail.com>
2022-09-20 09:53:42 +0300

    Setup laravel ui auth controllers, vue.js tooling

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
 import './bootstrap';
+import { createApp } from 'vue';
+
+import router from './router/index';
+import App from './components/App.vue';
+
+const app = createApp({});
+
+app.component('App', App);
+app.use(router).mount('#app');
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/{any}', [App\Http\Controllers\AppController::class, 'index'])->where('any', '.*')->name('home');
